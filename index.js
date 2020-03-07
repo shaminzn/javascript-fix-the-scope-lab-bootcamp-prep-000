eae4b7800bb6a022ef3bf2892b4f0eaab4159096
Shamira Naznin <shaminzn@gmail.com>
2020-03-07 21:23:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,21 +22,13 @@ const two = 2
   return n + two
 }
 
-// var funkyFunction = function() {
-//   return function() {
-//     return "FUNKY!"
-//   }
-// }
-//
-// // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// // NOTE: you only need to modify the code below this line.
-// var theFunk = funkyFunction
-// theFunk('FUNKY!');
-
-function() {
-  var funkyFunction = function() {
+var funkyFunction = function() {
+  return function() {
     return "FUNKY!"
   }
 }
 
+// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
+// NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction
+theFunk('FUNKY!');
